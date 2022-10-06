d85b2b4b6ed46a197372ef3389ab904679cdd48c
Ahmed Mohamed <ahmedmohamed@Ahmeds-MacBook-Pro.local>
2022-10-06 16:24:34 -0400

    Set up tailwind and tested it with basic styling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Styles from "./index.css";
 
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function Index() {
   return (
-    <div>Index</div>
-  )
+    <div className="absolute m-0 overflow-hidden top-0 bottom-0 w-full bg-gradient-to-r from-blurple to-nurple">
+      <div className="flex flex-col h-screen">
+        <h1 className="p-10 font-semibold">
+          <span className="text-3xl text-pink">ethereum</span>
+          <span className="text-2xl pl-3 text-white">multi-sender</span>
+        </h1>
+
+        <div className="h-full flex justify-center items-center">
+          <div className="max-w-[600px] bg-blurple rounded-md">
+            <p className="p-40 py-60">test</p>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Index
+root.render(<Index />);
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        blurple: "#060930",
+        nurple: "#333456",
+        purple: "#595B83",
+        pink: "#F4ABC4",
+      },
+    },
+  },
+  plugins: [],
+};
