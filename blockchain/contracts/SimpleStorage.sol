b651a9f7cff2179bcd6eb47f8c8ff7e29659f0b4
Ahmed Mohamed <ahmedmohamed@Ahmeds-MacBook-Pro.local>
2022-10-12 19:19:52 -0400

    Initiated Truffle Suite to test the smart contract on a local blockchain environment and deploy it

---
+++ blockchain/contracts/SimpleStorage.sol
@@ -0,0 +1,14 @@
+// SimpleStorage.sol
+pragma solidity ^0.8.17;
+
+contract SimpleStorage {
+  uint storedData;
+
+  function set(uint x) public {
+    storedData = x;
+  }
+
+  function get() public view returns (uint) {
+    return storedData;
+  }
+}
